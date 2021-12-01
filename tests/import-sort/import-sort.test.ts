e0d0b64c15d6521dca83a38cb4bcb6a17ca0f9c1
Zhuang Quan <zhuangquan@mokahr.com>
2021-12-01 17:47:52 +0800

    format

--- tests/import-sort/import-sort.test.ts
+++ tests/import-sort/import-sort.test.ts
@@ -7,43 +7,67 @@ import * as plugin from '../../src';
 import { prettierOption } from '../common';
 
 describe('import sort', () => {
-    test('import order sort', () => {
-        const code: string = readFileSync(resolve(__dirname, './order-sort/unformatted.ts'), 'utf8');
-        const expected: string = readFileSync(resolve(__dirname, './order-sort/formatted.ts'), 'utf8');
-        const actual: string = format(code, merge(prettierOption, {
-            parser: 'typescript',
-            plugins: [plugin],
-            importOrderSeparation: false,
-            importOrderSortSpecifiers: false,
-        }) as PrettierOptions);
+  test('import order sort', () => {
+    const code: string = readFileSync(resolve(__dirname, './order-sort/unformatted.ts'), 'utf8');
+    const expected: string = readFileSync(resolve(__dirname, './order-sort/formatted.ts'), 'utf8');
+    const actual: string = format(
+      code,
+      merge(prettierOption, {
+        parser: 'typescript',
+        plugins: [plugin],
+        configuredRules: ['importSort'],
+        importOrderSeparation: false,
+        importOrderSortSpecifiers: false,
+      }) as PrettierOptions
+    );
 
-        expect(actual).toBe(expected);
-    });
+    expect(actual).toBe(expected);
+  });
 
-    test('import order separation', () => {
-        const code: string = readFileSync(resolve(__dirname, './import-separation/unformatted.ts'), 'utf8');
-        const expected: string = readFileSync(resolve(__dirname, './import-separation/formatted.ts'), 'utf8');
-        const actual: string = format(code, merge(prettierOption, {
-            parser: 'typescript',
-            plugins: [plugin],
-            importOrderSeparation: true,
-            importOrderSortSpecifiers: false,
-        }) as PrettierOptions);
+  test('import order separation', () => {
+    const code: string = readFileSync(
+      resolve(__dirname, './import-separation/unformatted.ts'),
+      'utf8'
+    );
+    const expected: string = readFileSync(
+      resolve(__dirname, './import-separation/formatted.ts'),
+      'utf8'
+    );
+    const actual: string = format(
+      code,
+      merge(prettierOption, {
+        parser: 'typescript',
+        plugins: [plugin],
+        configuredRules: ['importSort'],
 
-        expect(actual).toBe(expected);
-    });
+        importOrderSeparation: true,
+        importOrderSortSpecifiers: false,
+      }) as PrettierOptions
+    );
 
-    test('specifiers sort', () => {
-        const code: string = readFileSync(resolve(__dirname, './specifiers-sort/unformatted.ts'), 'utf8');
-        const expected: string = readFileSync(resolve(__dirname, './specifiers-sort/formatted.ts'), 'utf8');
-        const actual: string = format(code, merge(prettierOption, {
-            parser: 'typescript',
-            plugins: [plugin],
-            importOrderSeparation: false,
-            importOrderSortSpecifiers: true,
-        }) as PrettierOptions);
+    expect(actual).toBe(expected);
+  });
 
-        expect(actual).toBe(expected);
-    });
+  test('specifiers sort', () => {
+    const code: string = readFileSync(
+      resolve(__dirname, './specifiers-sort/unformatted.ts'),
+      'utf8'
+    );
+    const expected: string = readFileSync(
+      resolve(__dirname, './specifiers-sort/formatted.ts'),
+      'utf8'
+    );
+    const actual: string = format(
+      code,
+      merge(prettierOption, {
+        parser: 'typescript',
+        plugins: [plugin],
+        configuredRules: ['importSort'],
+        importOrderSeparation: false,
+        importOrderSortSpecifiers: true,
+      }) as PrettierOptions
+    );
+
+    expect(actual).toBe(expected);
+  });
 });
-
