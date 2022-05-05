3418db17881525d4f180b17e1c300553352600a0
Zhuang Quan <zhuangquan@mokahr.com>
2022-05-05 17:58:03 +0800

    fix alias-substitution test case

--- tests/import-alias/alias-substitution/formatted.ts
+++ tests/import-alias/alias-substitution/formatted.ts
@@ -1,5 +1,5 @@
 import { REGS } from '@/constants';
 import { Button, Dropdown, Icon, Tooltip } from 'sugar-design';
 import * as sdf from '@SDFoundation';
-import Avatar from '../common/Avatar';
-import { ContainerLoading } from '../common/ContainerLoading';
+import Avatar from '@tests/import-alias/common/Avatar';
+import { ContainerLoading } from '@tests/import-alias/common/ContainerLoading';
